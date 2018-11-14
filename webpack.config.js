556c3a05428bd0d2db2cb50476caf73cc5f90189
lannav <sytnik.evhen@gmail.com>
2018-11-14 15:48:58 +0200

    181114_0348pm add select

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var path = require('path')
+var path = require('path');
 
 module.exports = {
     devtool: 'source-map',
@@ -16,7 +16,11 @@ module.exports = {
                 test: /\.js/,
                 loaders: ['babel-loader'],
                 include: path.join(__dirname, 'src')
+            },
+            {
+                test: /\.css/,
+                loaders: ['style-loader', 'css-loader'],
             }
         ]
     }
-}
+};
